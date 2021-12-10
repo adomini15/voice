0bfa591e711dfbbd4784bb5732059bc95ab613bc
adomini15 <adomini.edu@gmail.com>
2021-12-09 23:49:24 -0400

    SelectText Element Added

---
+++ src/components/diverse/SelectText.tsx
@@ -0,0 +1,23 @@
+import {IonList, IonLabel, IonItem} from "@ionic/react";
+
+const TextList =  () => {
+    return <IonList onClick={(e:any) => console.log(e.target?.textContent)}>
+        <IonItem>
+            <IonLabel>Pokémon Yellow</IonLabel>
+        </IonItem>
+        <IonItem>
+            <IonLabel>Mega Man X</IonLabel>
+        </IonItem>
+        <IonItem>
+            <IonLabel>The Legend of Zelda</IonLabel>
+        </IonItem>
+        <IonItem>
+            <IonLabel>Pac-Man</IonLabel>
+        </IonItem>
+        <IonItem>
+            <IonLabel>Super Mario World</IonLabel>
+        </IonItem>
+    </IonList>
+}
+
+export default TextList;
