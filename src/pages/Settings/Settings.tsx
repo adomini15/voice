8ca085c0b86fc41502dba1821b7bff103cd54549
adomini15 <adomini.edu@gmail.com>
2021-12-12 19:20:09 -0400

    Logout button added

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,7 +1,26 @@
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+// external
+import {IonAlert, IonButton, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+import {useDispatch} from "react-redux";
+
+// icons
+import { exit } from "ionicons/icons"
+
+// internal
+import {authLogoutRequested} from "../../actions/authActions";
+import {useState} from "react";
+
 
 const Settings: React.FC = () => {
-    return <IonPage>
+    const dispatch = useDispatch();
+
+    // local states
+    const [showLogoutConfirm, setShowLogoutConfirm] = useState(false);
+
+    const OnLogout = () => {
+        dispatch(authLogoutRequested())
+    }
+
+     return <IonPage>
         <IonHeader>
             <IonToolbar>
                 <IonTitle>
@@ -11,8 +30,28 @@ const Settings: React.FC = () => {
         </IonHeader>
 
         <IonContent fullscreen>
-
+            <IonButton expand="full" color="dark" onClick={ () => setShowLogoutConfirm(true) } >
+                <IonIcon icon={exit} />
+                Log out
+            </IonButton>
         </IonContent>
+
+        <IonAlert
+            isOpen={showLogoutConfirm}
+            onDidDismiss={() => setShowLogoutConfirm(false)}
+            header={'Confirm'}
+            message={'Are you sure to exit?'}
+            buttons={[{
+                text: 'Cancel',
+                role: 'cancel',
+            }, {
+                text: 'OK',
+                role: 'ok',
+                handler: () => {
+                    OnLogout()
+                }
+            }]}
+        />
     </IonPage>
 }
 
